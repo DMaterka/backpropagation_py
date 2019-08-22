34b99384ce241db9c110b8b0575a0a53a2e0215e
Daniel Materka <daniel.materka@gmail.com>
2019-08-22 23:32:31 +0200

    travis unittest

--- example1_test.py
+++ example1_test.py
@@ -1,18 +1,18 @@
 import unittest
 import backpropagation
 
-class TestCalculator(unittest.TestCase):
-    '''Testing the neural network structure'''
+
+class TestNetwork(unittest.TestCase):
+    """Testing the neural network structure"""
 
     def setUp(self):
-        '''Set up testing objects'''
+        """Set up testing objects"""
         self.a = 439
         self.b = 4
 
     def test_setting_input(self):
-        '''Testing add menthod'''
+        """Testing set/get menthod"""
         net = backpropagation.Net("1dimension", [[0.99, 0.99]], [[0.01]])
-        # print(calculator.add())
 
         self.assertEqual(net.getInputs(), [[0.99, 0.99]])
         
