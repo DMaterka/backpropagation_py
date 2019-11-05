d8191351ff8333c5edaaa1aa551e4355e8861e7d
Daniel Materka <daniel.materka@gmail.com>
2019-11-05 23:21:10 +0100

    refactor, new test

--- TestStorage.py
+++ TestStorage.py
@@ -19,17 +19,14 @@ class TestStorage(unittest.TestCase):
         conn.close()
         net = backpropagation.Net('xor.csv', 1)
         self.train_phase_output = train.train(net, "[3, 1]", 1000)
-        print("result is", self.train_phase_output.getLayer(len(self.train_phase_output.getLayers()) - 1).getValues())
+        print("result is", self.train_phase_output.get_results())
         net2 = backpropagation.Net('xor.csv', 1)
         self.predict_phase_output = predict.predict(net2)
-        print("The result is ", self.predict_phase_output.getLayer(len(self.predict_phase_output.getLayers()) - 1).getValues())
+        print("The result is ", self.predict_phase_output.get_results())
 
     def test_stored_network_equals_read_one(self):
         """Testing set/get method"""
-        self.assertAlmostEqual(
-            self.train_phase_output.getLayer(len(self.train_phase_output.getLayers()) - 1).getValues().all(),
-            self.predict_phase_output.getLayer(len(self.predict_phase_output.getLayers()) - 1).getValues().all()
-        )
+        self.assertAlmostEqual(self.train_phase_output.get_results(), self.predict_phase_output.get_results())
     
     def tearDown(self) -> None:
         os.remove('data/testing.db')
