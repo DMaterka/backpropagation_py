3ab587fece905f85ab898473f26b302bd6c7b3e9
Daniel Materka <daniel.materka@gmail.com>
2020-01-19 20:35:11 +0100

    refomat code into smaller pieces

--- train.py
+++ train.py
@@ -11,14 +11,33 @@ import re
 from operator import itemgetter
 
 
-def train(inputfile, structure, iterations, learning_rate, batch_size=1):
-    if structure != []:
-        structure = ast.literal_eval(structure)
+def prepare_net(hidden_structure, learning_rate, training_sets):
+    net = backpropagation.Net(inputfile, int(learning_rate))
+    if hidden_structure:
+        structure = ast.literal_eval(hidden_structure)
     else:
         structure = [3]
-
-    net = backpropagation.Net(inputfile, int(learning_rate))
+        
+    # prepend list with actual number of inputs
+    structure.insert(0, len(training_sets[0][0]))
+    # append list of hidden neurons structure with actual number of outputs
+    structure.append(len(training_sets[0][1]))
     
+    for index, value in enumerate(structure):
+        layer = backpropagation.Layer()
+        layer.setNeurons(np.zeros([value]))
+        # todo set weights randomly if none provided
+        if index > 0:
+            layer.setWeights(np.random.rand(value, structure[index - 1]))
+        
+        if index < len(structure) - 1:
+            # it seems that different bias values works better
+            layer.setBias(np.random.rand(structure[index + 1]))
+        net.setLayer(index, layer)
+    return net
+
+
+def prepare_training_sets(inputfile):
     df = pd.read_csv('data/' + inputfile)
     column_names = df.columns.values
     input_positions = []
@@ -28,48 +47,35 @@ def train(inputfile, structure, iterations, learning_rate, batch_size=1):
             input_positions.append(index)
         else:
             output_positions.append(index)
-
+    
     training_sets = df.iloc[:, :].values
     n_training_sets = []
-    #todo make training sets parsing on one go
+    # todo make training sets parsing on one go
     for ind, val in enumerate(training_sets):
         init_inputs = itemgetter(input_positions)(val)
         init_outputs = itemgetter(output_positions)(val)
         n_training_sets.insert(ind, [init_inputs, init_outputs])
      
-    # prepend list with actual number of inputs
-    structure.insert(0, len(input_positions))
-    # append list of hidden neurons structure with actual number of outputs
-    structure.append(len(output_positions))
-    
-    for index, value in enumerate(structure):
-        layer = backpropagation.Layer()
-        layer.setNeurons(np.zeros([value]))
-        #todo set weights randomly if none provided
-        if index > 0:
-            layer.setWeights(np.random.rand(value, structure[index-1]))
-        
-        if index < len(structure)-1:
-            # it seems that different bias values works better
-            layer.setBias(np.random.rand(structure[index+1]))
-        net.setLayer(index, layer)
+    return n_training_sets
 
+
+def train(net, iterations, training_sets, batch_size=1):
     #online training algorithm  with batch size=1 (one training set processed at once)
     learning_curve_data = []
     for i in range(0, int(iterations)):
-        modulo = divmod(i, len(n_training_sets))[1]
-        inputs, outputs = n_training_sets[modulo]
+        modulo = divmod(i, len(training_sets))[1]
+        inputs, outputs = training_sets[modulo]
         net.getLayer(0).setNeurons(inputs.T, False)
         net.setExpectedResults([outputs])
         net.backPropagate()
-        total_error = net.calculateTotalError(n_training_sets)
+        total_error = net.calculateTotalError(training_sets)
         print(total_error)
         learning_curve_data.append(total_error)
     
     visualise.print_learning_curve(learning_curve_data)
-    visualise.print_decision_regions(n_training_sets, net)
+    visualise.print_decision_regions(training_sets, net)
     
-    total_error = net.calculateTotalError(n_training_sets)
+    total_error = net.calculateTotalError(training_sets)
     results = dbops.get_model_results(inputfile)
     
     if results is None:
@@ -118,9 +124,11 @@ if __name__ == "__main__":
             iterations = arg
         elif opt in ("-s", "--struc"):
             structure = arg
-            
-    net = train(inputfile, structure, iterations, learning_rate, batch_size)
     
-    visualise.print_network(net)
+    training_sets = prepare_training_sets(inputfile)
+    net = prepare_net(structure, learning_rate, training_sets)
+    trained_net = train(net, iterations, training_sets, batch_size)
     
-    print("result is", net.get_results())
+    visualise.print_network(trained_net)
+    
+    print("result is", trained_net.get_results())
