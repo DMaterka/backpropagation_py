1541f553eea10509addf35d8dd5ab44283b91fcf
Daniel Materka <daniel.materka@gmail.com>
2020-01-29 00:45:06 +0100

    storage object

--- test/feature/exampleXOR.py
+++ test/feature/exampleXOR.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import train
-from src import visualise, dbops
+from src import visualise
 import dotenv
 
 if __name__ == "__main__":
@@ -19,9 +19,6 @@ if __name__ == "__main__":
     net.getLayer(1).setWeights([[.15, .2], [.25, .3]]).setBias([.6, .6])
     net.getLayer(2).setWeights([[.4, .45], [.5, .55]])
     
-    if not os.path.isfile('test/data/' + os.environ['DB_NAME']):
-        dbops.createSchema(os.environ['DB_NAME'])
-    dbops.save_net(net, 100, 'initial')
     trained_net = train.perform_training(net, iterations, training_sets, inputfile, batch_size)
     
     visualise.print_network(trained_net)
