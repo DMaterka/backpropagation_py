84e0e2d69dfe70e1f73ba5346562c3a21444bda2
Daniel Materka <daniel.materka@gmail.com>
2020-02-13 23:15:31 +0100

    fix storage tests

--- test/TestStorage.py
+++ test/TestStorage.py
@@ -2,6 +2,7 @@ import unittest
 import train
 from src import dbops
 import dotenv
+import numpy.testing as np_test
 
 
 class TestStorage(unittest.TestCase):
@@ -20,14 +21,41 @@ class TestStorage(unittest.TestCase):
         self.net.getLayer(1).setWeights([[.15, .2], [.25, .3]]).setBias([.6, .6])
         self.net.getLayer(2).setWeights([[.4, .45], [.5, .55]])
         dbOpsObject.save_net(self.net, 100, self.model_name)
-
-    def test_stored_network_equals_read_one(self):
         dbOpsObject = dbops.DbOps()
-        loaded_net = dbOpsObject.load_net(self.model_name)
+        self.loaded_net = dbOpsObject.load_net(self.model_name)
+
+    def test_first_layer_bias_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(0).getBias().getWeights(),
+                         self.loaded_net.getLayer(0).getBias().getWeights())
+
+    def test_first_layer_values_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(0).getValues(), self.loaded_net.getLayer(0).getValues())
+    
+    def test_first_layer_sums_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(0).getSums(), self.loaded_net.getLayer(0).getSums())
+
+    def test_second_layer_bias_equals_read_one(self):
         # test if first layer bias are the same
-        first_net_first_layer_weights = self.net.getLayer(0).getBias().getWeights()
-        second_net_first_layer_weights = loaded_net.getLayer(0).getBias().getWeights()
-        self.assertEqual(first_net_first_layer_weights.any(), second_net_first_layer_weights.any())
+        np_test.assert_allclose(self.net.getLayer(1).getBias().getWeights(),
+                         self.loaded_net.getLayer(1).getBias().getWeights())
+        
+    def test_second_layer_weights_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(1).getWeights(), self.loaded_net.getLayer(1).getWeights())
+    
+    def test_second_layer_sums_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(1).getSums(), self.loaded_net.getLayer(1).getSums())
+        
+    def test_second_layer_values_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(1).getValues(), self.loaded_net.getLayer(1).getValues())
+        
+    def test_third_layer_weights_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(2).getWeights(), self.loaded_net.getLayer(2).getWeights())
+
+    def test_third_layer_sums_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(2).getSums(), self.loaded_net.getLayer(2).getSums())
+
+    def test_third_layer_values_equals_read_one(self):
+        np_test.assert_allclose(self.net.getLayer(2).getValues(), self.loaded_net.getLayer(2).getValues())
     
     def tearDown(self) -> None:
         dbOpsObject = dbops.DbOps()
